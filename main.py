3fed7205c274c95b263016ac42fd23580db5339e
Prasadchaskar <chaskarprasad2000@gmail.com>
2024-02-24 09:15:54 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.chestCancerClf import logger
+from chestCancerClf import logger
+from chestCancerClf.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to chestCancerClf")
+STAGE_NAME = "Data Ingestion stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
