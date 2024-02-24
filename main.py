fab039ebb08d5ac4ad197471bfdf5016824cbc2e
Prasadchaskar <chaskarprasad2000@gmail.com>
2024-02-24 10:15:14 +0530

    prepare base model component

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from chestCancerClf import logger
 from chestCancerClf.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from chestCancerClf.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -10,5 +11,16 @@ if __name__ == '__main__':
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
         logger.exception(e)
         raise e
